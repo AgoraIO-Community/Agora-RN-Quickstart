2b31fa1efd4b77f7a817d7ea87af89b138eb57eb
Ekaansh Arora <blue.name68@gmail.com>
2020-04-22 22:52:31 +0530

    Initial commit

--- components/Video.js
+++ components/Video.js
@@ -1,40 +1,22 @@
+/* eslint-disable consistent-this */
 /* eslint-disable prettier/prettier */
 import requestCameraAndAudioPermission from './permission';
 import React, { Component } from 'react';
-import { View, NativeModules, Text, TouchableOpacity, Platform } from 'react-native';
-import { RtcEngine, AgoraView } from 'react-native-agora';
+import { View, Text, TouchableOpacity, Platform } from 'react-native';
+import RtcEngine, { RtcLocalView, RtcRemoteView } from 'react-native-agora';
 import styles from './Style';
 
-const { Agora } = NativeModules;            //Define Agora object as a native module
-
-const {
-  FPS30,
-  AudioProfileDefault,
-  AudioScenarioDefault,
-  Adaptative,
-} = Agora;                                  //Set defaults for Stream
-
-const config = {                            //Setting config of the app
-  appid: 'ENTER APP ID HERE',               //Enter the App ID generated from the Agora Website
-  channelProfile: 0,                        //Set channel profile as 0 for RTC
-  videoEncoderConfig: {                     //Set Video feed encoder settings
-    width: 720,
-    height: 1080,
-    bitrate: 1,
-    frameRate: FPS30,
-    orientationMode: Adaptative,
-  },
-  audioProfile: AudioProfileDefault,
-  audioScenario: AudioScenarioDefault,
-};
+let LocalView = RtcLocalView.SurfaceView;
+let RemoteView = RtcRemoteView.SurfaceView;
+let engine;
 
 class Video extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       peerIds: [],                                       //Array for storing connected peers
-      uid: Math.floor(Math.random() * 100),              //Generate a UID for local user
-      appid: config.appid,
+      appid: '9383ec2f56364d478cefc38b0a37d8bc',
       channelName: 'channel-x',                        //Channel Name for the current session
       joinSucceed: false,                                //State variable for storing success
     };
@@ -44,47 +26,55 @@ class Video extends Component {
       });
     }
   }
+
   componentDidMount() {
-    RtcEngine.on('userJoined', (data) => {
-      const { peerIds } = this.state;                   //Get currrent peer IDs
-      if (peerIds.indexOf(data.uid) === -1) {           //If new user has joined
-        this.setState({
-          peerIds: [...peerIds, data.uid],              //add peer ID to state array
+    let self = this;
+    /**
+    * @name init
+    * @description Function to initialize the Rtc Engine, attach event listeners and actions
+    */
+    async function init() {
+      engine = await RtcEngine.create(self.state.appid);
+      engine.enableVideo();
+
+      engine.addListener('UserJoined', (data) => {          //If user joins the channel
+        const { peerIds } = self.state;                     //Get currrent peer IDs
+        if (peerIds.indexOf(data) === -1) {                 //If new user
+          self.setState({ peerIds: [...peerIds, data] });   //add peer ID to state array
+        }
+      });
+
+      engine.addListener('UserOffline', (data) => {                 //If user leaves
+        self.setState({
+          peerIds: self.state.peerIds.filter(uid => uid !== data), //remove peer ID from state array
         });
-      }
-    });
-    RtcEngine.on('userOffline', (data) => {             //If user leaves
-      this.setState({
-        peerIds: this.state.peerIds.filter(uid => uid !== data.uid), //remove peer ID from state array
       });
-    });
-    RtcEngine.on('joinChannelSuccess', (data) => {                   //If Local user joins RTC channel
-      RtcEngine.startPreview();                                      //Start RTC preview
-      this.setState({
-        joinSucceed: true,                                           //Set state variable to true
+
+      engine.addListener('JoinChannelSuccess', (data) => {          //If Local user joins RTC channel
+        self.setState({ joinSucceed: true });                       //Set state variable to true
       });
-    });
-    RtcEngine.init(config);                                         //Initialize the RTC engine
+    }
+    init();
   }
+
   /**
   * @name startCall
   * @description Function to start the call
   */
   startCall = () => {
-    RtcEngine.joinChannel(this.state.channelName, this.state.uid);  //Join Channel
-    RtcEngine.enableAudio();                                        //Enable the audio
+    this.setState({ joinSucceed: true }); //Set state variable to true
+    engine.joinChannel(null, this.state.channelName, null, 0);  //Join Channel using null token and channel name
   }
+
   /**
   * @name endCall
   * @description Function to end the call
   */
   endCall = () => {
-    RtcEngine.leaveChannel();
-    this.setState({
-      peerIds: [],
-      joinSucceed: false,
-    });
+    engine.leaveChannel();
+    this.setState({ peerIds: [], joinSucceed: false });
   }
+
   /**
   * @name videoView
   * @description Function to return the view for the app
@@ -111,47 +101,47 @@ class Video extends Component {
                     this.state.peerIds.length > 3                   //view for four videostreams
                       ? <View style={styles.full}>
                         <View style={styles.halfViewRow}>
-                          <AgoraView style={styles.half}
-                            remoteUid={this.state.peerIds[0]} mode={1} />
-                          <AgoraView style={styles.half}
-                            remoteUid={this.state.peerIds[1]} mode={1} />
+                          <RemoteView style={styles.half} channelId={this.state.channelName}
+                            uid={this.state.peerIds[0]} renderMode={1} />
+                          <RemoteView style={styles.half} channelId={this.state.channelName}
+                            uid={this.state.peerIds[1]} renderMode={1} />
                         </View>
                         <View style={styles.halfViewRow}>
-                          <AgoraView style={styles.half}
-                            remoteUid={this.state.peerIds[2]} mode={1} />
-                          <AgoraView style={styles.half}
-                            remoteUid={this.state.peerIds[3]} mode={1} />
+                          <RemoteView style={styles.half} channelId={this.state.channelName}
+                            uid={this.state.peerIds[2]} renderMode={1} />
+                          <RemoteView style={styles.half} channelId={this.state.channelName}
+                            uid={this.state.peerIds[3]} renderMode={1} />
                         </View>
                       </View>
                       : this.state.peerIds.length > 2                   //view for three videostreams
                         ? <View style={styles.full}>
-                          <View style={styles.half}>
-                            <AgoraView style={styles.full}
-                              remoteUid={this.state.peerIds[0]} mode={1} />
+                          <View style={styles.half} channelId={this.state.channelName}>
+                            <RemoteView style={styles.full}
+                              uid={this.state.peerIds[0]} renderMode={1} />
                           </View>
                           <View style={styles.halfViewRow}>
-                            <AgoraView style={styles.half}
-                              remoteUid={this.state.peerIds[1]} mode={1} />
-                            <AgoraView style={styles.half}
-                              remoteUid={this.state.peerIds[2]} mode={1} />
+                            <RemoteView style={styles.half} channelId={this.state.channelName}
+                              uid={this.state.peerIds[1]} renderMode={1} />
+                            <RemoteView style={styles.half} channelId={this.state.channelName}
+                              uid={this.state.peerIds[2]} renderMode={1} />
                           </View>
                         </View>
                         : this.state.peerIds.length > 1                   //view for two videostreams
                           ? <View style={styles.full}>
-                            <AgoraView style={styles.full}
-                              remoteUid={this.state.peerIds[0]} mode={1} />
-                            <AgoraView style={styles.full}
-                              remoteUid={this.state.peerIds[1]} mode={1} />
+                            <RemoteView style={styles.full}
+                              uid={this.state.peerIds[0]} renderMode={1} />
+                            <RemoteView style={styles.full}
+                              uid={this.state.peerIds[1]} renderMode={1} />
                           </View>
                           : this.state.peerIds.length > 0                   //view for videostream
-                            ? <AgoraView style={styles.full}
-                              remoteUid={this.state.peerIds[0]} mode={1} />
+                            ? <RemoteView style={styles.full}
+                              uid={this.state.peerIds[0]} renderMode={1} />
                             : <View>
                               <Text style={styles.noUserText}> No users connected </Text>
                             </View>
                   }
-                  <AgoraView style={styles.localVideoStyle}
-                    zOrderMediaOverlay={true} showLocalVideo={true} mode={1} />
+                  <LocalView style={styles.localVideoStyle}               //view for local videofeed
+                    channelId={this.state.channelName} renderMode={1} zOrderMediaOverlay={true} />
                 </View>
             }
           </View>
